bfe20820d7a1459da8fecfdb7be8bf95992eccdf
Bogdan <bogdan.pavliv@gmail.com>
2024-11-18 16:59:14 +0200

    add project

--- src/app/components/headerComponent/links/navLink/NavLink.tsx
+++ src/app/components/headerComponent/links/navLink/NavLink.tsx
@@ -1,19 +1,24 @@
 "use client";
 
 import React from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import { usePathname } from "next/navigation";
+
+import { toggleMenu, closeMenu } from '../../../../store/headerSlice';
 
 type Props = {
     item: { title: string; path: string; }; 
 }
 
 const NavLink = ({item}: Props) => {
+    const dispatch = useDispatch();
     const pathName = usePathname();
   return (
     <li className="header__nav-item">
-      <Link className={`header__navLink ${pathName === item.path && "active"}`} href={item.path}>{item.title}</Link>
+      <Link className={`header__navLink ${pathName === item.path && "active"}`} href={item.path} onClick={() => dispatch(closeMenu())}>
+       {item.title}
+      </Link>
     </li>
   )
 }
